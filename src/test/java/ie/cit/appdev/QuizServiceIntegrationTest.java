0a421d1897f6f120f2f66e4d5eb3c121109a6383
gearyk <gearyk@gmail.com>
2015-05-11 09:54:14 +0100

    Added some simple Quiz Service Integration Tests. Coverage at 83%

---
+++ src/test/java/ie/cit/appdev/QuizServiceIntegrationTest.java
@@ -0,0 +1,59 @@
+package ie.cit.appdev;
+
+import java.util.List;
+
+import ie.cit.appdev.domain.Account;
+import ie.cit.appdev.service.AccountService;
+import ie.cit.appdev.domain.Question;
+import ie.cit.appdev.service.QuizService;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:beans.xml")
+public class QuizServiceIntegrationTest {
+	
+	@Autowired
+	QuizService qs;
+	List<Question> getQuiz;
+	List<Question> getRandom;
+	
+	
+	@Test @Transactional
+	public void testGetQuestions(){
+		getQuiz =qs.getQuizQuestions();
+		Assert.assertTrue("Verify the list contains 10 questions", getQuiz.size()==10);
+	}
+	
+	@Test @Transactional
+	public void testSetQuiz(){
+		getRandom =qs.setQuiz();
+		Assert.assertTrue("Verify the random list contains 10 questions", getRandom.size()==10);
+	}
+	
+	@Test @Transactional
+	public void testGetScore(){
+		String score =qs.getScore();
+		Assert.assertNotNull(score);
+	}
+	
+	@Test @Transactional
+	public void testUpdateAnswerResult(){
+		
+		qs.updateAnswerResult("attempt", "answer", "ID123");
+		
+		
+		
+	}
+	
+
+
+}
